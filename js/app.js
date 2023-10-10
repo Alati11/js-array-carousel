c98de9f6413841811ea5ca3fd0fcea3221ec2d2b
Alati11 <peppealati@gmail.com>
2023-10-10 18:07:24 +0200

    error template literal!!!

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 
 // - creare un array con le foto
-// dichiarare una variabile
-let arrayFoto = [ 'foto1', 'foto2', 'foto3', 'foto4', 'foto5']
+let arrayPhoto = [ '01.webp', '02.webp', '03.webp', '04.webp', '05.webp']
 
+// dichiararo variabili
+let container = '';
+// let itemThumb = '';
 
 // determeniare la lunghezza degli elementi contenuti 
-let lunghezzaArray = arrayFoto.length;
+let lunghezzaArray = arrayPhoto.length;
 
 // -crare un ciclo for per concatenare un template
     // -leggere i dati presenti nell'array
     // -concatenare i dati
     // -stampare il risultato della concatenazione
-
-for (let i = 0; i > lunghezzaArray; i++) {
-    console.log(i, arrayFoto);
-    console.log('ciao')
+for (let i = 0; i < lunghezzaArray; i++) {
+    // console.log(i,);
+   
+    container += `<div class="item"><img src="./img/${ arrayPhoto[i] }"></div>`;
+    // itemThumb += `<div class="thumb"><img src="./img/${ arrayPhoto[i] }"></div>`; 
+    console.log(i, container)
 }
+
+// const items_slider = document.querySelector('.item-slider').innerHTML = container;
+// const items_thumbnails = document.querySelector('.item-thumbnails').innerHTML = item_thumb;
